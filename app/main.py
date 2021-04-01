c78319929bc64ec1e29dd390ea61d08cfc9749eb
Frank-Howd <frankl.howd@gmail.com>
2021-04-01 12:05:23 -0400

    Deploy to Heroku

--- app/main.py
+++ app/main.py
@@ -26,7 +26,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=['*'],
     allow_credentials=True,
-    allow_methods=['GET', 'POST'],
+    allow_methods=['POST', 'PUT'],
     allow_headers=['*'],
 )
 
