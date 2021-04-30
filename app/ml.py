6e53e4ca0e192674136ae2f862b6c4b0051e0d2e
bconn315 <77983756+bconn315@users.noreply.github.com>
2021-04-29 21:10:50 -0500

    Add files via upload

--- app/ml.py
+++ app/ml.py
@@ -1,90 +1,103 @@
-"""Machine learning functions."""
+"""Machine learning functions"""
 
 import logging
-
-from joblib import load
-from fastapi import APIRouter
 import pandas as pd
-from pydantic import BaseModel, Field
+import random
+import spotipy
+
+from fastapi import APIRouter
+from joblib import load
+from os import getenv
+from pydantic import BaseModel, Field, validator
+from spotipy.oauth2 import SpotifyClientCredentials
 
 log = logging.getLogger(__name__)
 router = APIRouter()
 
-# uvicorn app.main:app --reload
+# Spotify credentials - this needs to be changed to environment variables
+SPOTIFY_API_KEY = 'b480cf11cde74d1b8d645b3f86f38d8b'
+SPOTIFY_API_KEY_SECRET = 'd0ed275c85fe48e19bf016c90d83221d'
 
-classifier = load("app/classifier.joblib")
+# load an instance of the spotipy class
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=SPOTIFY_API_KEY,
+                                                           client_secret=SPOTIFY_API_KEY_SECRET))
 
+# load our knn model
+classifier = load("knn_model.joblib")
 
+# provided scaffolding class
 class Item(BaseModel):
     """Use this data model to parse the request body JSON."""
 
-    category: str = Field(..., example="Food")
-    main_category: str = Field(..., example="Drink")
-    backers: int = Field(..., example=25)
-    usd_goal_real: float = Field(..., example=5000.00)
-    ks_length: int = Field(..., example=60)
+    artist: str = Field(..., example="Foo Fighters")
+    track: str = Field(..., example="Everlong")
+
 
     def to_df(self):
         """Convert pydantic object to pandas dataframe with 1 row."""
         return pd.DataFrame([dict(self)])
 
-    # @validator('x3')
-    # def x3_must_be_positive(cls, value):
-    #     """Validate that x3 is a positive number."""
-    #     assert value > 0, f'x3 == {value}, must be > 0'
-    #     return value
+    # new method for making Spotify queries using spotipy
+    def to_query(self):
+        """Convert pydantic object to Spotify query string"""
+        convert2dict = dict(self)
+        convert2list = " ".join([f"{k}:{v}" for k, v in convert2dict.items()])
+        return convert2list
 
 
-@router.post("/predict")
+@router.post('/predict')
 async def predict(item: Item):
     """
-    Determine the liklihood of success for a proposed Kickstarter campaign 🔮.
+    Return k-nearest neighbors based on user input - artist & song 🔮
 
     ### Request Body
-    - `category`: Select a category ['3D Printing', 'Academic', 'Accessories', 'Action', 'Animals', 'Animation', 'Anthologies', 
-        'Apparel', 'Apps', 'Architecture', 'Art', 'Art Books', 'Audio', 'Bacon', 
-        'Blues', 'Calendars', 'Camera Equipment', 'Candles', 'Ceramics', 'Childrens Books', 'Childrenswear', 
-        'Chiptune', 'Civic Design', 'Classical Music', 'Comedy', 'Comic Books', 'Comics', 'Community Gardens', 
-        'Conceptual Art', 'Cookbooks', 'Country & Folk', 'Couture', 'Crafts', 'Crochet', 'Dance', 
-        'Design', 'Digital Art', 'DIY', 'DIY Electronics', 'Documentary', 'Drama', 'Drinks', 'Electronic Music', 
-        'Embroidery', 'Events', 'Experimental', 'Fabrication Tools', 'Faith', 'Family', 'Fantasy', 
-        'Farmers Markets', 'Farms', 'Fashion', 'Festivals', 'Fiction', 'Film & Video', 'Fine Art', 
-        'Flight', 'Food', 'Food Trucks', 'Footwear', 'Gadgets', 'Games', 'Gaming Hardware', 
-        'Glass', 'Graphic Design', 'Graphic Novels', 'Hardware', 'Hip-Hop', 'Horror', 'Illustration', 
-        'Immersive', 'Indie Rock', 'Installations', 'Interactive Design', 'Jazz', 'Jewelry', 'Journalism', 
-        'Kids', 'Knitting', 'Latin', 'Letterpress', 'Literary Journals', 'Literary Spaces', 'Live Games', 
-        'Makerspaces', 'Metal', 'Mixed Media', 'Mobile Games', 'Movie Theaters', 'Music', 'Music Videos', 
-        'Musical', 'Narrative Film', 'Nature', 'Nonfiction', 'Painting', 'People', 'Performance Art', 
-        'Performances', 'Periodicals', 'Pet Fashion', 'Photo', 'Photobooks', 'Photography', 'Places', 
-        'Playing Cards', 'Plays', 'Poetry', 'Pop', 'Pottery', 'Print', 'Printing', 
-        'Product Design', 'Public Art', 'Publishing', 'Punk', 'Puzzles', 'Quilts', 'R&B', 
-        'Radio & Podcasts', 'Ready-to-wear', 'Residencies', 'Restaurants', 'Robots', 'Rock', 'Romance', 
-        'Science Fiction', 'Sculpture', 'Shorts', 'Small Batch', 'Software', 'Sound', 'Space Exploration', 
-        'Spaces', 'Stationery', 'Tabletop Games', 'Taxidermy', 'Technology', 'Television', 'Textiles', 
-        'Theater', 'Thrillers', 'Translations', 'Typography', 'Vegan', 'Video', 'Video Art', 
-        'Video Games', 'Wearables', 'Weaving', 'Web', 'Webcomics', 'Webseries', 'Woodworking', 
-        'Workshops', 'World Music', 'Young Adult', 'Zines']
-
-    - `main_category`: Select a main category ['Art', 'Comics', 'Crafts', 'Dance', 'Design', 'Fashion', 'Film & Video', 
-        'Food', 'Games', 'Journalism', 'Music', 'Photography', 'Publishing', 'Technology', 'Theater']
-
-    - `backers`: Estimate the number of expected backers for your Kickstarter
-        project
-
-    - `usd_goal_real`: Enter prospective Kickstarter campaign's fundraising
-        goal in U.S. dollars
-
-    - `ks_length`: Enter the duration of proposed Kickstarter fundraising
-        campaign in days
+    - `artist`: Enter a band or artist name here 
+    - `song`: Enter a song recorded by the artist 
 
     ### Response
-    - `prediction`: success, failed
-    - `predict_proba`: float between 0.0 and 1.0,
-    representing the predicted class's probability
+    - `prediction`: 10 song / artist parings based on user input
 
+    What a great way to discover new music!
     """
+    # import our .csv (database)
+    df = pd.read_csv('app/spotify_rock.csv', index_col=0)
+    # turn pydantic object into a searchable string
+    query = item.to_query()
+    # grab search user_input search results from Spotify
+    search_results = sp.search(q=query, type="track", limit=1)
+    # grab the song ID so we can search it's features
+    search_id = search_results['tracks']['items'][0]['id']
+    # grab the name of the song (I've been doing this just for verifictation during buildout)
+    name = search_results['tracks']['items'][0]['name']
+    # grab the release date - it's a model input
+    year = int(search_results['tracks']['items'][0]['album']['release_date'][:4])
+    # search user_input song features
+    search_features = sp.audio_features(tracks=search_id)
+    # create dictionary to pass into 'to_df'
+    song_cluster = {'duration_ms': search_features[0]['duration_ms'],
+                    'explicit': 0,
+                    'release_date': year,
+                    'danceability': search_features[0]['danceability'],
+                    'energy': search_features[0]['energy'],
+                    'key': search_features[0]['key'],
+                    'loudness': search_features[0]['loudness'],
+                    'mode': search_features[0]['mode'],
+                    'speechiness': search_features[0]['speechiness'],
+                    'acousticness': search_features[0]['acousticness'],
+                    'instrumentalness': search_features[0]['instrumentalness'],
+                    'liveness': search_features[0]['liveness'],
+                    'valence': search_features[0]['valence'], 
+                    'tempo': search_features[0]['tempo'],
+                    'time_signature': search_features[0]['time_signature']}
+    # create single row dataframe to pass into our KNN model
+    X_song_cluster = pd.DataFrame([dict(song_cluster)])
+    # turn our dataframe into a series object
+    series = X_song_cluster.iloc[0, :].to_numpy()
+    # query our model for 10 nearest neighbors of user's input song
+    neighbors = classifier.kneighbors([series], return_distance=False)
+    # index into return object
+    answers = df.iloc[neighbors[0], [1, 2]]
+    # return answers
+    return answers
 
-    X_new = item.to_df()
-    choice = classifier.predict(X_new)
-    probability = classifier.predict_proba(X_new)
-    return choice[0], f"{probability[0][1]*100:.2f}% probability"
+    # uvicorn app.main:app --reload
